dbff73c630b905e7919594c651c218067f491bd2
unknown <mithun.k@gradious.com>
2025-09-24 13:31:30 +0530

    notes 1

--- One.java
+++ One.java
@@ -1,3 +1,30 @@
 public class One {
+  // static int cnt = 0;
+
+  static void hello(int cnt) {
+    if (cnt >= 5) {
+      return;
+    }
+    hello(cnt + 2);
+    System.out.println("a " + " " + ++cnt);
+    hello(cnt + 1);
+    System.out.println("b " + " " + --cnt);
+  }
+
+  static int sn(int num)
+  {
+    if( num == 0)
+    {
+      return 0;
+    }
+    int e = sn(num - 1);
+    int d = e + num;
+    return d; 
+  }
   
+  public static void main(String[] args) {
+    int s = sn(3);
+    System.out.println(s);
+    
+  }
 }
