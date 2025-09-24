1e1ad426406ae7a9ffbf72665abdfd5f8d4d05da
unknown <mithun.k@gradious.com>
2025-09-24 15:49:31 +0530

    notes1

--- One.java
+++ One.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class One {
   // static int cnt = 0;
 
@@ -11,8 +14,7 @@ public class One {
     System.out.println("b " + " " + --cnt);
   }
 
-  static int sn(int num)
-  {
+  static int sn(int num) {
     if (num == 0) {
       return 0;
     }
@@ -21,13 +23,11 @@ public class One {
     return d;
   }
 
-  static int helperForword(int num)
-  {
+  static int helperForword(int num) {
     return snForword(num, 0);
   }
 
-  static int snForword(int num, int sm)
-  {
+  static int snForword(int num, int sm) {
     if (num == 0) {
       return sm;
     }
@@ -35,17 +35,69 @@ public class One {
     int e = snForword(num - 1, m);
     return e;
   }
-  
-  static String rev(String s)
-  {
+
+  static String revBack(String s, int ind) {
+    // if( s.length() == ind)
+    // {
+    // return "";
+    // }
+    if (s.length() - 1 == ind) {
+      return s.charAt(ind) + "";
+    }
+
+    String f = revBack(s, ind + 1);
+    String ans = f + s.charAt(ind);
+    return ans;
+  }
+
+  static String revFront(String s, int ind, int ans) {
     return "";
   }
-  
-  public static void main(String[] args) {
-    // int d = helperForword(5);
-    String s = "abcdef";
-    String c = rev(s);
-    System.out.println(c); // "fedcba"
-    
+
+  static void swap(char arr[], int a, int b) {
+    char c = arr[a];
+    arr[a] = arr[b];
+    arr[b] = c;
   }
+
+  static void permutate(char arr[], int work, int len) {
+
+    if (work == len)
+    {
+      System.out.println(new String(arr));
+      return; 
+    }
+
+    for (int i = work; i < len; i++) 
+    {
+      swap(arr, work, i);
+      permutate(arr, work + 1, len);
+      swap(arr, work, i);
+    }
+
+    return; 
+  }
+
+  public static void main(String[] args) {
+    String s = "abcdef";
+    char arr[] = s.toCharArray();
+    permutate(arr, 0, arr.length);
+    /*
+     * abc
+     * acb
+     * bac
+     * bca
+     * cab
+     * cba
+     * 
+     * 
+     */
+
+    // // int d = helperForword(5);
+    // String s = "abcdef";
+    // String c = revBack(s, 0);
+    // System.out.println(c); // "fedcba"
+
+  }
+
 }
